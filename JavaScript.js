e6ee741bcdc0ae0d36d844686c983caf036cb36e
Akhilstrings11 <125636435+Akhilstrings11@users.noreply.github.com>
2024-02-13 01:18:38 +0530

    akhil

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+// function displayFormOutput() {
+
+//     var name = document.getElementById('uName').value;
+//     var email = document.getElementById('email').value;
+//     var phone = document.getElementById('phone').value;
+//     var password = document.getElementById('password').value;
+
+//     console.log("Form OutPut:-");
+//     console.log("Name:- " + name);
+//     console.log("email:- " + email);
+//     console.log("hone:- " + phone);
+//     console.log("Password:- " + password);
+
+//     return false;
+// }
